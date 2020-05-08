02e6474f97179939824e51ac697d9bd9265f9942
Pedro Arthur <pedro.art.vel@gmail.com>
2020-05-08 18:57:10 -0300

    set interval to button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import appStyles from './App.module.css';
+import Typed from 'react-typed';
+
+import Buttons from './Components/Buttons.module.css';
+
 
 class App extends Component {
 
+  state = { show : false }
+
+  timer = () => {
+    this.timerBtn = setInterval(() => {
+      this.setState({ show : true })
+    }, 2200)
+  }
+
   render() {
+
+    const show = this.state.show;
+
+    this.timer();
 
     return (
       <div className= {appStyles.App} >
         <img src={logo} className= {appStyles.AppLogo} alt="logo" />
-        <h1>Hi, I'm the Name Handler!</h1>
+        <br></br>
+
+        <h1>
+          <Typed
+              strings={['Hi, I\'m the Name Handler!']}
+              typeSpeed={60}
+          />
+        </h1>
+
+        {show &&
+          <button className={Buttons.buttonPrimary} >
+            Cool, but what are you, exactly?
+          </button>
+        }
+
       </div>
     );
 
